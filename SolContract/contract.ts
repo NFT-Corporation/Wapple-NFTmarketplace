ad476acf1a264a25853fe2ebdae1b62f92bec78a
unknown <montecristodev0516@gmail.com>
2024-08-06 04:54:48 +0200

    add code

--- SolContract/contract.ts
+++ SolContract/contract.ts
@@ -18,3 +18,24 @@ export const Home: NextPage = props => {
     </ConnectionProvider>
   );
 };
+
+import { NextPage } from "next";
+import { FC, ReactNode } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import * as web3 from "@solana/web3.js";
+ 
+export const Home: NextPage = props => {
+  const endpoint = web3.clusterApiUrl("devnet");
+  const wallets = useMemo(() => [], []);
+ 
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <p>Put the rest of your app here</p>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
