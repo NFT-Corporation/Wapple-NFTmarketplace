5aefe1b8143ceb500d65025c08317a92998215a9
unknown <montecristodev0516@gmail.com>
2024-08-06 04:23:28 +0200

    updated contract

--- SolContract/keypair.ts
+++ SolContract/keypair.ts
@@ -41,3 +41,24 @@ console.log(
     `💸 Finished! Sent ${LAMPORTS_TO_SEND} to the address ${toPubkey}. `,
   );
   console.log(`Transaction signature is ${signature}!`);
+
+  const transaction = new Transaction();
+   
+  const LAMPORTS_TO_SEND = 5000;
+   
+  const sendSolInstruction = SystemProgram.transfer({
+    fromPubkey: senderKeypair.publicKey,
+    toPubkey,
+    lamports: LAMPORTS_TO_SEND,
+  });
+   
+  transaction.add(sendSolInstruction);
+   
+  const signature = await sendAndConfirmTransaction(connection, transaction, [
+    senderKeypair,
+  ]);
+   
+  console.log(
+    `💸 Finished! Sent ${LAMPORTS_TO_SEND} to the address ${toPubkey}. `,
+  );
+  console.log(`Transaction signature is ${signature}!`);
