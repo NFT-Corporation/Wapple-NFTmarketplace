32001188b36f28ea660a6e0727be00518b8ef5c7
unknown <montecristodev0516@gmail.com>
2024-08-06 04:27:48 +0200

    delete part

--- SolContract/keypair.ts
+++ SolContract/keypair.ts
@@ -16,28 +16,3 @@ const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
 console.log(
   `✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`,
 );
-
-console.log(
-    `✅ Loaded our own keypair, the destination public key, and connected to Solana`,
-  );
-   
-  const transaction = new Transaction();
-   
-  const LAMPORTS_TO_SEND = 5000;
-   
-  const sendSolInstruction = SystemProgram.transfer({
-    fromPubkey: senderKeypair.publicKey,
-    toPubkey,
-    lamports: LAMPORTS_TO_SEND,
-  });
-   
-  transaction.add(sendSolInstruction);
-   
-  const signature = await sendAndConfirmTransaction(connection, transaction, [
-    senderKeypair,
-  ]);
-   
-  console.log(
-    `💸 Finished! Sent ${LAMPORTS_TO_SEND} to the address ${toPubkey}. `,
-  );
-  console.log(`Transaction signature is ${signature}!`);
